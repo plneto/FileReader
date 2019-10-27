66a07eb18ea7a6589f50c8105bef999365880e70
Paulo Lemos Neto <plneto@gmail.com>
2019-10-27 18:45:10 +0000

    Add support for reading encrypted XML files

---
+++ src/FileReader/Models/EncryptedXmlFile.cs
@@ -0,0 +1,14 @@
+﻿using FileReader.Constants;
+
+namespace FileReader.Models
+{
+    internal class EncryptedXmlFile : IFile
+    {
+        public EncryptedXmlFile()
+        {
+            FilePath = FilePaths.EncryptedXmlFilePath;
+        }
+
+        public string FilePath { get; }
+    }
+}
